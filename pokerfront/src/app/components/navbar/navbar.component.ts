7bede37874fcf40aa020cd9dcd4539a0fabc6512
Oleksandr Chumak <ochumak@eagleprojects.it>
2025-06-02 20:23:00 +0200

    Fixed the navbar for angular material

--- pokerfront/src/app/components/navbar/navbar.component.ts
+++ pokerfront/src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, Router } from '@angular/router';
-import { HostListener } from '@angular/core';
 import { MaterialModule } from '../../material.module';
 import { LoginService } from '../../service/login.service';
 
@@ -12,22 +11,11 @@ import { LoginService } from '../../service/login.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-
 export class NavbarComponent {
-  activeDropdown: string | null = null;
-
   constructor(
     private router: Router,
     private loginService: LoginService
   ) {}
-
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: MouseEvent) {
-    const navbar = (event.target as HTMLElement).closest('.navbar');
-    if (!navbar) {
-      this.closeDropdown();
-    }
-  }
 
   isLoggedIn(): boolean {
     return this.loginService.isTokenValid();
@@ -35,20 +23,6 @@ export class NavbarComponent {
 
   getUserRole(): string | null {
     return this.loginService.getUserRole();
-  }
-
-  toggleDropdown(menu: string): void {
-    // If clicking the same menu that's already open, close it
-    if (this.activeDropdown === menu) {
-      this.activeDropdown = null;
-    } else {
-      // If clicking a different menu, open it (and close any other open menu)
-      this.activeDropdown = menu;
-    }
-  }
-
-  closeDropdown(): void {
-    this.activeDropdown = null;
   }
 
   login(): void {
